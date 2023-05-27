1161909654f3a7830ad58bc095f45cc550909467
mouhcineakroud <mouhcine.akroud.40@gmail.com>
2023-05-27 18:10:16 +0000

    fix getline

--- getline.c
+++ getline.c
@@ -10,46 +10,7 @@
  */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-	static char buffer[BUFFER_SIZE];
-	static ssize_t buffer_index;
-	static ssize_t bytes_read;
-	size_t line_length = 0;
-	char *line = NULL;
-	char current_char;
-
-	while (1)
-	{
-		if (buffer_index >= bytes_read)
-		{
-			bytes_read = read(fileno(stream), buffer, BUFFER_SIZE);
-			buffer_index = 0;
-			if (bytes_read <= 0)
-				break;
-		}
-		current_char = buffer[buffer_index++];
-		if (current_char == '\n' || current_char == '\0')
-		{
-			if (*n < ++line_length)
-			{
-				*n = line_length;
-				line = realloc(line, *n);
-			}
-			line[line_length - 1] = '\0';
-			break;
-		}
-		else
-		{
-			line_length++;
-			if (*n < line_length)
-			{
-				*n = line_length * 2;
-				line = realloc(line, *n);
-			}
-			line[line_length - 1] = current_char;
-		}
-	}
-	*lineptr = line;
-	return (line_length == 0 ? -1 : (ssize_t)line_length);
+	return (getline(lineptr, n, stream));
 }
 /**
  * _isanumber - check if number
--- help3.c
+++ help3.c
@@ -1,26 +1,21 @@
 #include "main.h"
-
-/*
-int _getenv(char *key)
-{
-	char *token;
-	unsigned int i = 0;
-
-	for (i = 0; environ[i]; i++)
-	{
-		token = strtok(environ[i], "=");
-		if (strcmp(key, token) == 0)
-			return (1);
-	}
-	return (0);
-}
-*/
-
+/**
+ * _setenv - Entry point.
+ * @key: key value.
+ * @value: value.
+ * @overwrite: overwrite.
+ *
+ * Return: Always 0.
+ */
 void _setenv(char *key, char *value, int overwrite)
 {
 	setenv(key, value, overwrite);
 }
-
+/**
+ * _unsetenv - Entry point.
+ * @key: key value.
+ * Return: Always 0.
+ */
 void _unsetenv(char *key)
 {
 	unsetenv(key);
